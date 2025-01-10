8a62e1109cdfa3cd6358fa4351b3b0948a624108
jakubpulaczewski <yellowbrown4@gmail.com>
2025-01-10 16:10:07 +0000

    Updated integration tests for loaders.py

--- tests/integration/test_integration_loaders.py
+++ tests/integration/test_integration_loaders.py
@@ -1,12 +1,13 @@
 # Standard Imports
-
+from types import FunctionType
 # Third Party Imports
 import pytest
 
 
 # Project Imports
 from core.loaders import PluginLoader
-from core.models.phases import PipelinePhase, IExtractor, ILoader
+from core.models.phases import PipelinePhase
+
 from plugins.registry import PluginRegistry
 
 
@@ -16,32 +17,13 @@ class TestIntegrationPluginLoader:
     def plugin_loader(self) -> PluginLoader:
         self.loader = PluginLoader()
 
-    @pytest.mark.asyncio
-    async def test_load_custom_single_plugin(self):
-        # Ensure the registry is empty before loading the plugin
-        assert PluginRegistry._registry == {}
-        
-        # Load the custom plugin
-        plugin = {'tests/resources/plugins/custom_extractor.py'}
-        self.loader.load_custom_plugins(plugin)
-
-        # Get the loaded custom plugin and run extract data
-        plugin_class = PluginRegistry._registry[PipelinePhase.EXTRACT_PHASE]["custom_extract"]
-        extracted_data = await plugin_class(id='custom_plugin').extract_data()
-
-
-        assert issubclass(plugin_class, IExtractor)
-        assert extracted_data == 'Pandas S3 Loaded Data'
-
-    
     def test_load_custom_multiple_plugins(self):
         # Ensure the registry is empty before loading the plugin
         assert PluginRegistry._registry == {}
 
         # Load the custom plugin
         plugins = {
-             '/workspaces/workflow/tests/resources/plugins/custom_extractor.py',
-             '/workspaces/workflow/tests/resources/plugins/custom_loader.py'
+             '/workspaces/workflow/tests/resources/custom_plugins.py',
         }
         self.loader.load_custom_plugins(plugins)
 
@@ -49,10 +31,15 @@ class TestIntegrationPluginLoader:
         extractor_plugin = PluginRegistry._registry[PipelinePhase.EXTRACT_PHASE]["custom_extract"]
         loader_plugin = PluginRegistry._registry[PipelinePhase.LOAD_PHASE]["custom_load"]
 
+        assert len(PluginRegistry._registry.keys()) == 2
 
-        assert issubclass(extractor_plugin, IExtractor)
-        assert issubclass(loader_plugin, ILoader)
-    
+        assert extractor_plugin.__name__ == 'custom_extractor'
+        assert type(extractor_plugin) == FunctionType
+
+        assert loader_plugin.__name__ == 'custom_loader'
+        assert type(loader_plugin) == FunctionType
+
+
     # def test_load_plugins_success():
     #     #TODO: Create a test for this.
     #     ...
---
+++ tests/resources/custom_plugins.py
@@ -0,0 +1,27 @@
+# Standard Imports
+from functools import wraps
+# Third Party Imports
+
+
+# Project Imports
+from plugins.registry import plugin
+from core.models.phases import PipelinePhase
+
+
+
+
+@plugin(PipelinePhase.EXTRACT_PHASE, "custom_extract")
+def custom_extractor():
+    @wraps(custom_extractor)
+    async def inner():
+        return "CUSTOM EXTRACTED DATA"
+    return inner
+
+
+
+@plugin(PipelinePhase.LOAD_PHASE, "custom_load")
+def custom_loader():
+    @wraps(custom_loader)
+    async def inner(data):
+        return None
+    return inner
