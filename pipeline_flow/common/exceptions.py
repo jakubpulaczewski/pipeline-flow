ab92a82a4983c52eb6621e5017534ca32a654ef0
jakubpulaczewski <21315783+jakubpulaczewski@users.noreply.github.com>
2025-06-26 21:20:45 +0000

    Added a class decorator for custome exceptions

--- pipeline_flow/common/exceptions.py
+++ pipeline_flow/common/exceptions.py
@@ -1,34 +1,46 @@
 # Standard Imports
 from __future__ import annotations
 
-from typing import Self
+import functools
+from typing import Any, Self
 
 # Third Party Imports
 
 # Project Imports
 
 
-class PipelineBaseError(Exception):
-    """A base exception class for any pipeline errors."""
+def docstring_message(cls: Any) -> Any:  # noqa: ANN401
+    """Decorates an exception to make its docstring its default message.
 
-    def __init__(self: Self, message: str, original_exception: Exception | None = None) -> None:
-        self.message = message
-        self.original_exception = original_exception
-        super().__init__(self.message)
+    - https://stackoverflow.com/a/66491013/13618168
+    """
+    cls_init = cls.__init__
 
-    def __str__(self: Self) -> str:
-        if self.original_exception:
-            return f"{self.message} | Caused by: {self.original_exception}"
-        return self.message
+    @functools.wraps(cls.__init__)
+    def wrapped_init(
+        self: Self,
+        msg: str | dict | None = None,
+        *args: tuple,
+        **kwargs: dict[str, Any],
+    ) -> None:
+        err_message: str = self.__doc__ if not msg else f"{self.__doc__}\nDetails: {msg}"
+        cls_init(self, err_message, *args, **kwargs)
 
-
-class ExtractError(PipelineBaseError): ...
-
-
-class TransformError(PipelineBaseError): ...
-
-
-class LoadError(PipelineBaseError): ...
+    cls.__init__ = wrapped_init
+    return cls
 
 
-class TransformLoadError(PipelineBaseError): ...
+@docstring_message
+class ExtractError(Exception): ...
+
+
+@docstring_message
+class TransformError(Exception): ...
+
+
+@docstring_message
+class LoadError(Exception): ...
+
+
+@docstring_message
+class TransformLoadError(Exception): ...
